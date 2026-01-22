77c6847d9ea1afda309702fa6e9029124556f8ec
Md. Sakib Hasan <245582512+SakibHasan077@users.noreply.github.com>
2026-01-22 17:49:20 +0600

    Implement calculator functionality with button event listeners

--- script.js
+++ script.js
@@ -20,3 +20,36 @@ window.addEventListener("click", (el) => {
   }
 });
 //* End Model Pop-up
+//* Calculator App
+let display = document.getElementById("display");
+let items = document.querySelectorAll("button");
+let string = "";
+let itemArray = Array.from(items);
+let isResultShown = false;
+itemArray.forEach((btn) => {
+  btn.addEventListener("click", (el) => {
+    if (el.target.innerHTML === "DEL") {
+      string = string.substring(0, string.length - 1);
+      display.value = string;
+      isResultShown = false;
+    } else if (el.target.innerHTML === "C") {
+      string = " ";
+      display.value = string;
+      isResultShown = false;
+    } else if (el.target.innerHTML == "=") {
+      string = eval(string);
+      display.value = string;
+      isResultShown = true;
+    } else {
+      if (isResultShown && !isNaN(el.target.innerHTML)) {
+        string = el.target.innerHTML;
+        display.value = string;
+        isResultShown = false;
+      } else {
+        string = string + el.target.innerHTML;
+        display.value = string;
+        isResultShown = false;
+      }
+    }
+  });
+});
